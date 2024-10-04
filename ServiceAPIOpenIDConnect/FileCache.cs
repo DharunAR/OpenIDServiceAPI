502732b2a947e6133617d117e657530a4189d780
dharunkumara <dharunkumara@opentext.com>
2024-10-04 10:15:45 +0530

    Add project files.

---
+++ ServiceAPIOpenIDConnect/FileCache.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceAPIOpenIDConnect
+{
+    public class FileCache
+    {
+        public static FileCache GetUserCache(string cacheFilePath)
+        {
+            return new FileCache(cacheFilePath);
+        }
+
+        private readonly string CacheFilePath;
+        private readonly object FileLock = new object();
+
+        private FileCache(string cacheFilePath)
+        {
+            CacheFilePath = cacheFilePath;
+        }
+
+        public void BeforeAccessNotification(TokenCacheNotificationArgs args)
+        {
+            try
+            {
+                lock (FileLock)
+                {
+                    args.TokenCache.DeserializeMsalV3(File.Exists(CacheFilePath)
+                        ? File.ReadAllBytes(CacheFilePath)
+                        : null);
+                }
+            }
+            catch (MsalClientException)
+            {
+                lock (FileLock)
+                {
+                    //The cache file deserialization would have failed, create a new token cache.
+                    File.Delete(CacheFilePath);
+                }
+            }
+        }
+
+        public void AfterAccessNotification(TokenCacheNotificationArgs args)
+        {
+            // if the access operation resulted in a cache update
+            if (args.HasStateChanged)
+            {
+                lock (FileLock)
+                {
+                    // Reflect changes in the persistent store
+                    File.WriteAllBytes(CacheFilePath, args.TokenCache.SerializeMsalV3());
+                }
+            }
+        }
+    }
+}
---
+++ ServiceAPIOpenIDConnect/Program.cs
@@ -0,0 +1,89 @@
+﻿using HP.HPTRIM.Service.Client;
+using HP.HPTRIM.ServiceModel;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceAPIOpenIDConnect
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                string accessToken = msalAuthentication();
+                TrimClient trimClient = new TrimClient("https://localhost/cmserviceapi");
+                trimClient.AddHeader("Authorization", $"Bearer {accessToken}");
+
+                //GET
+                PostSearch postSearch = new PostSearch()
+                {
+                    q = $"unique:DharunOT",
+                    TrimType = BaseObjectTypes.Location,
+                    pageSize = 10,
+                    Properties = new List<string>() { "LocationNickName", "NameString" }
+                };
+                var response = trimClient.Get<LocationsResponse>(postSearch);
+
+                //POST a record
+
+                Record record = new Record()
+                {
+                    Title = "testOpenID",
+                    RecordType = new RecordTypeRef()
+                    {
+                        Uri = 9000000017
+                    }
+                };
+                var recResponse = trimClient.Post<RecordsResponse>(record);
+
+                Console.WriteLine(recResponse.Results[0].Title);
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+        }
+        private static string msalAuthentication()
+        {
+            var appsettings = ConfigurationManager.AppSettings;
+            string[] scopes = new string[] { appsettings["scope"] };
+            var pca = PublicClientApplicationBuilder.Create(appsettings["clientID"]).
+                    WithAuthority(appsettings["authority"])
+                    .Build();
+
+            //Token Cache
+            FileCache cacheHelper = FileCache.GetUserCache(Path.Combine(appsettings["tokenCachePath"] ,"TokenCache.dat"));
+            pca.UserTokenCache.SetBeforeAccess(cacheHelper.BeforeAccessNotification);
+            pca.UserTokenCache.SetAfterAccess(cacheHelper.AfterAccessNotification);
+
+            var accounts = pca.GetAccountsAsync();
+            accounts.Wait();
+            AuthenticationResult authResult;
+            try
+            {
+                var silentToken = pca.AcquireTokenSilent(scopes, accounts.Result.FirstOrDefault()).ExecuteAsync();
+                silentToken.Wait();
+                authResult = silentToken.Result;
+            }
+            catch (AggregateException)
+            {
+                //Silent token is not available, let's fetch the token using interactive approach
+                var uu = pca.AcquireTokenInteractive(scopes)
+                    .ExecuteAsync();
+                authResult = uu.Result;
+            }
+            return authResult.AccessToken;
+        }
+
+    }
+}
